412dff6d7ca9d8e2c2ae4bc40d6de1d0c9491756
Tam <tam.jankowska@gmail.com>
2021-03-18 12:50:11 +0000

    Created models

--- pathfinder/phenomenon/models.py
+++ pathfinder/phenomenon/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Phenomena(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=255)
+    date_of_occ = models.DateField("Date of occurrence")
+    image = models.ImageField(upload_to="phenomenon/images")
+    tags = TaggableManager()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name}: {self:description[:30]}... "
