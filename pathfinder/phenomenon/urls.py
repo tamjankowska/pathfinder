43db807209bc908918bc2b2b84913fc38e808839
Tam <tam.jankowska@gmail.com>
2021-03-19 14:57:12 +0000

    fixed get_absolute_url

--- pathfinder/phenomenon/urls.py
+++ pathfinder/phenomenon/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.phenomena, name="phenomena"),
-    path("locations", views.locations, name="locations")
+    path("<int:phenomena_id>/", views.phenomena, name="phenomena"),
+    path("locations/", views.locations, name="locations")
 ]
